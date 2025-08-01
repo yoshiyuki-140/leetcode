fe766f87a2c8bc0892188144c5c82a6c6e63cf4c
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2025-08-01 13:37:17 +0900

    add : 20. Valid Parentheses's good ans

--- valid-parentheses/main.go
+++ valid-parentheses/main.go
@@ -1,59 +1,26 @@
 package validParentheses
 
-import (
-	"fmt"
-	"strings"
-)
-
-// スタック構造定義
-type Stack struct {
-	sData []string
-}
-
-func (pStack *Stack) Push(s string) {
-	pStack.sData = append(pStack.sData, s)
-}
-
-func (pStack *Stack) Pop() (string, error) {
-
-	sDataLength := len(pStack.sData)
-	if sDataLength <= 0 {
-		return "", fmt.Errorf("%s", "スタックにデータがありません.")
-	}
-
-	lastS := pStack.sData[sDataLength-1]
-	pStack.sData = pStack.sData[:sDataLength-1]
-
-	return lastS, nil
-}
-
 func isValid(s string) bool {
-	if len(s) == 0 {
-		return true
+	if len(s)%2 == 1 {
+		return false
 	}
-	sList := strings.Split(s, "")
-	stack := Stack{}
-	// 括弧チェック
-	for _, str := range sList {
-		switch str {
-		case "(", "{", "[":
-			stack.Push(str)
-		case ")":
-			popedStr, err := stack.Pop()
-			if err != nil || popedStr != "(" {
+
+	var stack []rune
+	for _, v := range s {
+		switch v {
+		case '(', '{', '[':
+			stack = append(stack, v) // スタックに詰める
+		case ')', '}', ']':
+			if len(stack) == 0 {
 				return false
 			}
-		case "}":
-			popedStr, err := stack.Pop()
-			if err != nil || popedStr != "{" {
-				return false
-			}
-		case "]":
-			popedStr, err := stack.Pop()
-			if err != nil || popedStr != "[" {
+			top := stack[len(stack)-1]   // スタックから取り出す
+			stack = stack[:len(stack)-1] //　後処理
+			if !((top == '(' && v == ')') || (top == '{' && v == '}') || (top == '[' && v == ']')) {
 				return false
 			}
 		}
 	}
-	return len(stack.sData) == 0
+	// forループ終了時点でスタックが空であれば、括弧はきちんと閉じられていると判断
+	return len(stack) <= 0
 }
