aa84ecae5b691a29fb9b5a8756bfd5b9fe1ed68d
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2025-08-02 06:39:17 +0900

    update : 118. Pascal's Triangle

--- pascals-triangle/main.go
+++ pascals-triangle/main.go
@@ -6,33 +6,25 @@ package pascalsTriangle
 */
 
 func generate(numRows int) [][]int {
-	/*
-		1. numRowsが1であれば、[[1]]を返却する
-		1. numRowsが2であれば、[[1],[1,1]]を返却する
-		1. numRows >= 3の時、resultArray:[][]intに[[1],[1,1]]を格納する.
-		1. for文でnumRows分ループをかける -> i:index の初期値は2
-			1. パスカルの三角形でいうと一つ上の段の配列から値を読みだして和をとる
-			1. j番目の格納先に和の結果を格納する
-			1. appendArrayができたらresultArrayにappendする
-		1. resultArrayを返す
-	*/
 	if numRows == 1 {
 		return [][]int{{1}}
 	}
 	if numRows == 2 {
 		return [][]int{{1}, {1, 1}}
 	}
+	resultArray := make([][]int, numRows)
 	// この行が読み取られた時点で numRows >= 3 が確定する
-	resultArray := [][]int{{1}, {1, 1}}
+	// resultArrayを[[1],[1,1]]の状態にする
+	resultArray[0] = []int{1}
+	resultArray[1] = []int{1, 1}
+
 	for i := 2; i < numRows; i++ {
-		appendArray := make([]int, i+1)
-		appendArray[0] = 1
-		appendArray[i] = 1
+		resultArray[i] = make([]int, i+1)
+		resultArray[i][0] = 1
+		resultArray[i][i] = 1
 		for j := 1; j < i; j++ {
-			appendArray[j] = resultArray[i-1][j-1] + resultArray[i-1][j]
-			// fmt.Printf("i:%d, j:%d, appendArray[j]:%d = resultArray[i-1][j-1]:%d + resultArray[i-1][j]:%d\n", i, j, appendArray[j], resultArray[i-1][j-1], resultArray[i-1][j])
+			resultArray[i][j] = resultArray[i-1][j-1] + resultArray[i-1][j]
 		}
-		resultArray = append(resultArray, appendArray)
 	}
 	return resultArray
 }
