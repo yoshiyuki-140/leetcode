b284601271ede26855a44d0694045947f8d55273
yoshiyuki-140 <moyashikuro1225@gmail.com>
2026-05-01 17:49:49 +0900

    feat:

--- lru-cache/lrucache/lru_cache.go
+++ lru-cache/lrucache/lru_cache.go
@@ -1,10 +1,21 @@
 package lrucache
 
+// NOTE: 双方向リストの定義
+// Node
+// - Prev *Node
+// - Next *Node
+
+// List
+// - Head *Node
+// - Tail *Node
+
+// 今回必要になる処理
+// -
+
 type LRUCache struct {
 }
 
 func Constructor(capacity int) LRUCache {
-
 	return LRUCache{}
 }
 
