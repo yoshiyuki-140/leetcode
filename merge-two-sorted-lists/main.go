45d2f193bc67f500583186f7976e95866da41f34
yoshiyuki-140 <moyashikuro1225@gmail.com>
2026-05-02 13:41:33 +0900

    feat:

--- merge-two-sorted-lists/main.go
+++ merge-two-sorted-lists/main.go
@@ -6,30 +6,29 @@ type ListNode struct {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	// 返却時に先頭を特定するためdummyを作る
 	dummy := &ListNode{}
 	current := dummy
 
-	// 値を比較して、小さいほうをcurrentに入れる処理
+	// Valを比較してcurrentに格納する
 	for list1 != nil && list2 != nil {
 		if list1.Val <= list2.Val {
 			current.Next = list1
-			list1 = list1.Next // list1の更新
+			list1 = list1.Next
 		} else {
 			current.Next = list2
-			list2 = list2.Next // listの更新
+			list2 = list2.Next
 		}
 		current = current.Next
 	}
 
-	// どちらかの要素がnilになったらnilではない要素をそのままcurrentに結合する
+	// nilではないほうをcurrentのNextにつなげる
 	if list1 != nil {
 		current.Next = list1
-	}
-	if list2 != nil {
+	} else {
 		current.Next = list2
 	}
 
-	// dummyのnextを返却する
+	// dummyのNextを返却する
+
 	return dummy.Next
 }
