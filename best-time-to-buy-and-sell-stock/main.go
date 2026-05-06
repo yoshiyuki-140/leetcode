8cc3c5ea6445b54fdf563771bea9e9618a3c0548
yoshiyuki-140 <moyashikuro1225@gmail.com>
2026-05-06 11:35:55 +0900

    feat:

--- best-time-to-buy-and-sell-stock/main.go
+++ best-time-to-buy-and-sell-stock/main.go
@@ -5,11 +5,16 @@ func maxProfit(prices []int) int {
 	maxP := 0
 	for sell < len(prices) {
 		if prices[buy] < prices[sell] {
+			// profit計算
 			profit := prices[sell] - prices[buy]
+			// maxPの更新可能判断と必要に応じた更新
 			maxP = max(maxP, profit)
 		} else {
+			// 利益が出なかった場合
+			// sellの場所にbuyを持っていく
 			buy = sell
 		}
+		// sellの更新
 		sell++
 	}
 	return maxP
