76c925311557648a633950538475d018554f656b
yoshiyuki-140 <moyashikuro1225@gmail.com>
2026-05-11 18:41:32 +0900

    feat:

--- valid-parentheses/main.go
+++ valid-parentheses/main.go
@@ -1,26 +1,40 @@
 package validparentheses
 
 func isValid(s string) bool {
+	// sの長さが奇数長だったらfalseを返却する
 	if len(s)%2 == 1 {
 		return false
 	}
 
+	// pairを定義
+	pair := map[rune]rune{
+		']': '[',
+		')': '(',
+		'}': '{',
+	}
+
+	// スタックの定義
 	var stack []rune
-	for _, v := range s {
-		switch v {
-		case '(', '{', '[':
-			stack = append(stack, v) // スタックに詰める
-		case ')', '}', ']':
+
+	for _, c := range s {
+		// 対応する括弧を呼び出す
+		if open, ok := pair[c]; ok {
+			// 1. スタックが空ならfalse
 			if len(stack) == 0 {
 				return false
 			}
-			top := stack[len(stack)-1]   // スタックから取り出す
-			stack = stack[:len(stack)-1] //　後処理
-			if !((top == '(' && v == ')') || (top == '{' && v == '}') || (top == '[' && v == ']')) {
+			// 2. スタックのトップが、期待される開き括弧ではないならfalse
+			if stack[len(stack)-1] != open {
 				return false
 			}
+			// 正しいペアになったので、スタックを更新
+			stack = stack[:len(stack)-1]
+		} else {
+			// 開き括弧の場合はスタックにPush
+			stack = append(stack, c)
 		}
 	}
-	// forループ終了時点でスタックが空であれば、括弧はきちんと閉じられていると判断
-	return len(stack) <= 0
+
+	// 最終的にスタックが空ならOK
+	return len(stack) == 0
 }
